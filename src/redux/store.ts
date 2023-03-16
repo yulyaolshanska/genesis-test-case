4e70b846c09f241bee81824c773fd38d1e5adad9
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-03-17 00:01:27 +0200

    store

---
+++ src/redux/store.ts
@@ -0,0 +1,35 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { authApi } from "./auth/authApi";
+import { persistedReducer } from "./auth/authPersist";
+import { coursesApi } from "./coursesApi";
+
+export const store = configureStore({
+  reducer: {
+    [coursesApi.reducerPath]: coursesApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
+
+    auth: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) => [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+    authApi.middleware,
+    coursesApi.middleware,
+  ],
+});
+
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
