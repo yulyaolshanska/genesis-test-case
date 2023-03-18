42453f524dd896ee65b84e2920a5108ed6b3508e
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-03-18 22:33:50 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import { persistor, store } from "redux/store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="genesis-test-case">
+    <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
